ccf093f102e2eab58d44f9875300eab85e3e929a
mottoslo <c4save@korea.ac.kr>
2023-05-23 00:26:48 +0900

    Chore : create enum methods and Dto

--- src/main/java/com/example/moyiza_be/club/controller/CreateClubController.java
+++ src/main/java/com/example/moyiza_be/club/controller/CreateClubController.java
@@ -2,6 +2,7 @@ package com.example.moyiza_be.club.controller;
 
 
 import com.example.moyiza_be.club.dto.CreateClubIdResponse;
+import com.example.moyiza_be.club.dto.createclub.*;
 import com.example.moyiza_be.club.service.CreateClubService;
 import com.example.moyiza_be.common.security.UserDetailsImpl;
 import com.example.moyiza_be.common.utils.Message;
@@ -9,6 +10,7 @@ import com.example.moyiza_be.user.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,10 +28,54 @@ public class CreateClubController {
         return createClubService.initCreateClubId(user);
     }
 
-    @RequestMapping("/club/create/{createclub-id}/category")
+    @RequestMapping("/club/create/{createclub_id}/category")
     public ResponseEntity<Message> setCategory(
-            @AuthenticationPrincipal UserDetailsImpl userDetails
+            @AuthenticationPrincipal UserDetailsImpl userDetails,
+            @RequestBody CreateRequestCategoryDto requestCategoryDto
     ){
-        return null;
+        User user = userDetails.getUser();
+        return createClubService.setCategory(user, requestCategoryDto.getCategoryEnum());
     }
+
+    @RequestMapping("/club/create/{createclub_id}/tag")
+    public ResponseEntity<Message> setTag(
+            @AuthenticationPrincipal UserDetailsImpl userDetails,
+            @RequestBody CreateRequestTagDto requestTagDto
+    ){
+        User user = userDetails.getUser();
+        return createClubService.setTag(user, requestTagDto.getTagEnumList());
+    }
+    
+    @RequestMapping("/club/create/{creatclub_id}/title")
+    public ResponseEntity<Message> setTitle(
+            @AuthenticationPrincipal UserDetailsImpl userDetails,
+            @RequestBody CreateRequestTitleDto requestTitleDto
+    ){
+        User user = userDetails.getUser();
+        return createClubService.setTitle(user, requestTitleDto.getTitle());
+    }
+
+    @RequestMapping("/club/create/{createclub_id}/content")
+    public ResponseEntity<Message> setContent(
+            @AuthenticationPrincipal UserDetailsImpl userDetails,
+            @RequestBody CreateRequestContentDto requestContentDto
+    ){
+        User user = userDetails.getUser();
+        return createClubService.setContent(user, requestContentDto.getContent());
+    }
+
+    @RequestMapping("/club/create/{createclub_id}/restriction")
+    public ResponseEntity<Message> setRestriction(
+            @AuthenticationPrincipal UserDetailsImpl userDetails,
+            @RequestBody CreateRequestPolicyDto requestPolicy
+    ){
+        User user = userDetails.getUser();
+        return createClubService.setPolicy(
+                user, requestPolicy.getAgePolicy(),requestPolicy.getGenderPolicy());
+    }
+
+    //완료 API부터 작성
+
+
+
 }
--- src/main/java/com/example/moyiza_be/common/enums/TagEnum.java
+++ src/main/java/com/example/moyiza_be/common/enums/TagEnum.java
@@ -5,9 +5,20 @@ public enum TagEnum {
     BASEBALL("야구"),
     WALKING("산책");
 
-    String tagEnum;
+    private final String tag;
 
-    TagEnum(String tagEnum) {
-        this.tagEnum = tagEnum;
+    TagEnum(String tag) {
+        this.tag = tag;
+    }
+
+    public String getTag(){return this.tag;}
+
+    public static TagEnum fromString(String tag) {
+        for (TagEnum tagEnum : TagEnum.values()) {
+            if (tagEnum.getTag().equalsIgnoreCase(tag)) {
+                return tagEnum;
+            }
+        }
+        throw new IllegalArgumentException(String.format("%s에 해당하는 Tag를 찾을 수 없습니다",tag));
     }
 }
