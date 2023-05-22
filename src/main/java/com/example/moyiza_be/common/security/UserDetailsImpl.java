c7bc17373b4e774c6f1ea57cfc1c0d3680bc62ac
donghui <eastshine0221@gmail.com>
2023-05-22 15:42:16 +0900

    WIP : club-create

--- src/main/java/com/example/moyiza_be/common/security/UserDetailsImpl.java
+++ src/main/java/com/example/moyiza_be/common/security/UserDetailsImpl.java
@@ -1,63 +1,63 @@
-//package com.example.moyiza_be.common.security;
-//
-//import com.example.moyiza_be.common.enums.UserRoleEnum;
-//import com.example.moyiza_be.user.entity.User;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.security.core.GrantedAuthority;
-//import org.springframework.security.core.authority.SimpleGrantedAuthority;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.stereotype.Component;
-//
-//import java.util.ArrayList;
-//import java.util.Collection;
-//
-//@Component
-//public class UserDetailsImpl implements UserDetails {
-//    private final User user;
-//
-//    public UserDetailsImpl(User user) {
-//        this.user = user;
-//    }
-//
-//    @Override
-//    public Collection<? extends GrantedAuthority> getAuthorities() {
-//        UserRoleEnum role = UserRoleEnum.ROLE_USER;
-//        String authority = role.getAuthorization();
-//
-//        SimpleGrantedAuthority simpleGrantedAuthority = new SimpleGrantedAuthority(authority);
-//        Collection<GrantedAuthority> authorities = new ArrayList<>();
-//        authorities.add(simpleGrantedAuthority);
-//        return authorities;
-//    }
-//
-//    @Override
-//    public String getPassword() {
-//        System.out.println("user.getPassword() = " + user.getPassword());
-//        return user.getPassword();
-//    }
-//
-//    @Override
-//    public String getUsername() { return this.user.getEmail(); }
-//
-//    @Override
-//    public boolean isAccountNonExpired() {
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean isAccountNonLocked() {
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean isCredentialsNonExpired() { return true; }
-//
-//    @Override
-//    public boolean isEnabled() {
-//        return true;
-//    }
-//
-//    public User getUser() {
-//        return this.user;
-//    }
-//}
+package com.example.moyiza_be.common.security;
+
+import com.example.moyiza_be.common.enums.UserRoleEnum;
+import com.example.moyiza_be.user.entity.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Component
+public class UserDetailsImpl implements UserDetails {
+    private final User user;
+
+    public UserDetailsImpl(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        UserRoleEnum role = UserRoleEnum.ROLE_USER;
+        String authority = role.getAuthorization();
+
+        SimpleGrantedAuthority simpleGrantedAuthority = new SimpleGrantedAuthority(authority);
+        Collection<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(simpleGrantedAuthority);
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        System.out.println("user.getPassword() = " + user.getPassword());
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() { return this.user.getEmail(); }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() { return true; }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public User getUser() {
+        return this.user;
+    }
+}
--- src/main/java/com/example/moyiza_be/common/security/UserDetailsServiceImpl.java
+++ src/main/java/com/example/moyiza_be/common/security/UserDetailsServiceImpl.java
@@ -1,22 +1,22 @@
-//package com.example.moyiza_be.common.security;
-//import com.example.moyiza_be.user.entity.User;
-//import com.example.moyiza_be.user.repository.UserRepository;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.core.userdetails.UsernameNotFoundException;
-//import org.springframework.stereotype.Component;
-//
-//@RequiredArgsConstructor
-//@Component
-//public class UserDetailsServiceImpl implements UserDetailsService {
-//    private final UserRepository userRepository;
-//    @Override
-//    public UserDetailsImpl loadUserByUsername(String email) throws UsernameNotFoundException {
-//
-//        User user = userRepository.findByEmail(email).orElseThrow(
-//                () -> new NullPointerException("유저를 찾을 수 없습니다")
-//        );
-//        return new UserDetailsImpl(user);
-//    }
-//}
-//
+package com.example.moyiza_be.common.security;
+import com.example.moyiza_be.user.entity.User;
+import com.example.moyiza_be.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepository userRepository;
+    @Override
+    public UserDetailsImpl loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        User user = userRepository.findByEmail(email).orElseThrow(
+                () -> new NullPointerException("유저를 찾을 수 없습니다")
+        );
+        return new UserDetailsImpl(user);
+    }
+}
+
