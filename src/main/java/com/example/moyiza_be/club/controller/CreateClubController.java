d8a42bf62d254f40089631b93e392dfd421684d8
mottoslo <c4save@korea.ac.kr>
2023-05-23 13:56:24 +0900

    feat : createClubService done // set Dto, method for ClubCreation

--- src/main/java/com/example/moyiza_be/club/controller/CreateClubController.java
+++ src/main/java/com/example/moyiza_be/club/controller/CreateClubController.java
@@ -1,8 +1,11 @@
 package com.example.moyiza_be.club.controller;
 
 
+import com.example.moyiza_be.club.dto.ClubResponseDto;
 import com.example.moyiza_be.club.dto.CreateClubIdResponse;
+import com.example.moyiza_be.club.dto.ResumeCreationDto;
 import com.example.moyiza_be.club.dto.createclub.*;
+import com.example.moyiza_be.club.entity.CreateClub;
 import com.example.moyiza_be.club.service.CreateClubService;
 import com.example.moyiza_be.common.security.userDetails.UserDetailsImpl;
 import com.example.moyiza_be.common.utils.Message;
@@ -10,9 +13,7 @@ import com.example.moyiza_be.user.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,62 +21,96 @@ import org.springframework.web.bind.annotation.RestController;
 public class CreateClubController {
     private final CreateClubService createClubService;
 
-    @RequestMapping("/club/create")
-    public ResponseEntity<CreateClubIdResponse> initCreateClubId(
+
+
+    @PostMapping("/club/create")
+    public ResponseEntity<?> initCreateClubId(
             @AuthenticationPrincipal UserDetailsImpl userDetails
     ) {
         User user = userDetails.getUser();
-        return createClubService.initCreateClubId(user);
+        /// user.getUserId 검증로직 (anonymous일 경우 getId exception처리)
+        return createClubService.initCreateClubId(user.getId());
     }
 
-    @RequestMapping("/club/create/{createclub_id}/category")
+
+    @GetMapping("/club/create/{createclub_id}")
+    public ResponseEntity<CreateClubIdResponse> getPreviousCreateClub(
+            @AuthenticationPrincipal UserDetailsImpl userDetails,
+            @PathVariable Long createclub_id
+    ){
+        User user = userDetails.getUser();
+        /// user.getUserId 검증로직 (anonymous일 경우 getId exception처리)
+        return createClubService.getPreviousCreateClub(user.getId(), createclub_id);
+    }
+
+    @PutMapping("/club/create/{createclub_id}/category")
     public ResponseEntity<Message> setCategory(
             @AuthenticationPrincipal UserDetailsImpl userDetails,
-            @RequestBody CreateRequestCategoryDto requestCategoryDto
+            @RequestBody CreateRequestCategoryDto requestCategoryDto,
+            @PathVariable Long createclub_id
     ){
         User user = userDetails.getUser();
-        return createClubService.setCategory(user, requestCategoryDto.getCategoryEnum());
+        return createClubService.setCategory(
+                user, createclub_id, requestCategoryDto.getCategoryEnum()
+        );
     }
 
-    @RequestMapping("/club/create/{createclub_id}/tag")
+    @PutMapping("/club/create/{createclub_id}/tag")
     public ResponseEntity<Message> setTag(
             @AuthenticationPrincipal UserDetailsImpl userDetails,
-            @RequestBody CreateRequestTagDto requestTagDto
+            @RequestBody CreateRequestTagDto requestTagDto,
+            @PathVariable Long createclub_id
     ){
 
         User user = userDetails.getUser();
-        return createClubService.setTag(user, requestTagDto.getTagEnumList());
+        return createClubService.setTag(
+                user, createclub_id, requestTagDto.getTagEnumList()
+        );
     }
     
-    @RequestMapping("/club/create/{creatclub_id}/title")
+    @PutMapping("/club/create/{createclub_id}/title")
     public ResponseEntity<Message> setTitle(
             @AuthenticationPrincipal UserDetailsImpl userDetails,
-            @RequestBody CreateRequestTitleDto requestTitleDto
+            @RequestBody CreateRequestTitleDto requestTitleDto,
+            @PathVariable Long createclub_id
     ){
         User user = userDetails.getUser();
-        return createClubService.setTitle(user, requestTitleDto.getTitle());
+        return createClubService.setTitle(
+                user, createclub_id, requestTitleDto.getTitle()
+        );
     }
 
-    @RequestMapping("/club/create/{createclub_id}/content")
+    @PutMapping("/club/create/{createclub_id}/content")
     public ResponseEntity<Message> setContent(
             @AuthenticationPrincipal UserDetailsImpl userDetails,
-            @RequestBody CreateRequestContentDto requestContentDto
+            @RequestBody CreateRequestContentDto requestContentDto,
+            @PathVariable Long createclub_id
     ){
         User user = userDetails.getUser();
-        return createClubService.setContent(user, requestContentDto.getContent());
+        return createClubService.setContent(
+                user, createclub_id, requestContentDto.getContent()
+        );
     }
 
-    @RequestMapping("/club/create/{createclub_id}/restriction")
-    public ResponseEntity<Message> setRestriction(
+    @PutMapping("/club/create/{createclub_id}/restriction")
+    public ResponseEntity<Message> setPolicy(
             @AuthenticationPrincipal UserDetailsImpl userDetails,
-            @RequestBody CreateRequestPolicyDto requestPolicy
+            @RequestBody CreateRequestPolicyDto requestPolicy,
+            @PathVariable Long createclub_id
     ){
         User user = userDetails.getUser();
         return createClubService.setPolicy(
-                user, requestPolicy.getAgePolicy(),requestPolicy.getGenderPolicy());
+                user, createclub_id, requestPolicy.getAgePolicy(),requestPolicy.getGenderPolicy());
     }
 
-    //완료 API부터 작성
+    @PostMapping("/club/create/{createclub_id}/confirm")
+    public ResponseEntity<ClubResponseDto> confirmCreation(
+            @AuthenticationPrincipal UserDetailsImpl userDetails,
+            @PathVariable Long createclub_id
+    ){
+        User user = userDetails.getUser();
+        return createClubService.confirmCreation(userDetails, createclub_id);
+    }
 
 
 
