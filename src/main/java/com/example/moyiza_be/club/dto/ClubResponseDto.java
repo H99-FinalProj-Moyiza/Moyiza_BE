4c6a3cb0c21211ede95366fd8352149f1c4e1253
mottoslo <c4save@korea.ac.kr>
2023-05-23 23:59:17 +0900

    feat : confirm creation

--- src/main/java/com/example/moyiza_be/club/dto/ClubResponseDto.java
+++ src/main/java/com/example/moyiza_be/club/dto/ClubResponseDto.java
@@ -6,29 +6,33 @@ import com.example.moyiza_be.common.enums.GenderEnum;
 import com.example.moyiza_be.common.enums.GenderPolicyEnum;
 import com.example.moyiza_be.common.enums.TagEnum;
 import com.example.moyiza_be.user.entity.User;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.Calendar;
 
+@Getter
+@NoArgsConstructor
 public class ClubResponseDto {
     private Long id;
     private Long ownerId;
     private String title;
-    private CategoryEnum category;
+    private String category;
     private String tagString;
     private String content;
     private Integer agePolicy;
-    private GenderPolicyEnum genderPolicy;
+    private String genderPolicy;
     private Integer maxGroupSize;
 
     public ClubResponseDto(Club club) {
         this.id = club.getId();
         this.ownerId = club.getOwnerId();
         this.title = club.getTitle();
-        this.category = club.getCategory();
+        this.category = club.getCategory().toString();
         this.tagString = club.getTagString();
         this.content = club.getContent();
         this.agePolicy = club.getAgePolicy();
-        this.genderPolicy = club.getGenderPolicy();
+        this.genderPolicy = club.getGenderPolicy().toString();
         this.maxGroupSize = club.getMaxGroupSize();
     }
 
