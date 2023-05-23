be9614085c146b1c4681c727e7e4c7a1fbb8ca92
Dunkin00 <meister630@gmail.com>
2023-05-24 04:09:23 +0900

    Feat : AWS S3 이미지 업로드 추가
    - 회원 정보 수정시 이미지 파일 업로드 기능 추가
    - 혼동 방지를 위해 User.java profileUrl -> profileImage로 변경
    - build.gradle s3 dependencies 추가

---
+++ src/main/java/com/example/moyiza_be/common/utils/AwsS3Uploader.java
@@ -0,0 +1,76 @@
+package com.example.moyiza_be.common.utils;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.UUID;
+@RequiredArgsConstructor
+@Service
+@Log4j2
+public class AwsS3Uploader {
+    private final AmazonS3 amazonS3;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    // 프로필 업로드
+    public String uploadFile(MultipartFile multipartFile){
+        // 파일 이름이 같으면 저장이 안돼서 파일이름 앞에 UUID를 붙인다.
+        String fileName = UUID.randomUUID() + "_" + multipartFile.getOriginalFilename();
+        ObjectMetadata objMeta = new ObjectMetadata();
+        objMeta.setContentType(multipartFile.getContentType());
+        objMeta.setContentLength(multipartFile.getSize());
+
+        try(InputStream inputStream = multipartFile.getInputStream()){
+            amazonS3.putObject(bucket, fileName, multipartFile.getInputStream(), objMeta);
+        } catch (IOException e){
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "파일 업로드에 실패했습니다.");
+        }
+        return amazonS3.getUrl(bucket, fileName).toString();
+    }
+
+    public String upload(File uploadFile, String dirName) {
+        String fileName = dirName + "/" + uploadFile.getName();
+        String uploadImageUrl = putS3(uploadFile, fileName);
+        removeNewFile(uploadFile);
+        return uploadImageUrl;
+    }
+
+    // 로컬에 저장된 파일 삭제
+    private void removeNewFile(File targetFile) {
+        if (targetFile.delete()) {
+            log.info("파일이 삭제되었습니다.");
+        } else {
+            log.info("파일이 삭제되지 못했습니다.");
+        }
+    }
+
+    private String putS3(File uploadFile, String fileName) {
+        amazonS3.putObject(new PutObjectRequest(bucket, fileName, uploadFile).withCannedAcl(CannedAccessControlList.PublicRead));
+        return amazonS3.getUrl(bucket, fileName).toString();
+    }
+
+    // 프로필 수정 기존 이미지 삭제
+    public boolean delete(String fileUrl) {
+        try {
+            String[] temp = fileUrl.split("/");
+            String fileKey = temp[temp.length-1];
+            amazonS3.deleteObject(bucket, fileKey);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
