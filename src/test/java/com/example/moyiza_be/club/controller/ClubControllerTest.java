e4ec686eb1a10295f25adf4feeeded153b942e90
Hohomii <glgltjsal@naver.com>
2023-05-24 17:14:59 +0900

    Test : CI Test2

---
+++ src/test/java/com/example/moyiza_be/club/controller/ClubControllerTest.java
@@ -0,0 +1,12 @@
+package com.example.moyiza_be.club.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClubControllerTest {
+
+    @Test
+    void joinClub() {
+    }
+}
