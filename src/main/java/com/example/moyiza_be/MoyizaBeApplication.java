c64a1e052e51005768b79dc818ac4fcbe9400a2a
강재형 <c4save@korea.ac.kr>
2023-05-24 22:01:37 +0900

    feat : jasypt added // new : application.yml

--- src/main/java/com/example/moyiza_be/MoyizaBeApplication.java
+++ src/main/java/com/example/moyiza_be/MoyizaBeApplication.java
@@ -1,11 +1,13 @@
 package com.example.moyiza_be;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableEncryptableProperties
 public class MoyizaBeApplication {
 
     public static void main(String[] args) {
