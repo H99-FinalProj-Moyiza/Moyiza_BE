0e05909e8859cbf5367c4f6ff11b923884ec743a
강재형 <c4save@korea.ac.kr>
2023-05-24 18:48:19 +0900

    for merge

--- src/main/java/com/example/moyiza_be/common/security/config/WebSecurityConfig.java
+++ src/main/java/com/example/moyiza_be/common/security/config/WebSecurityConfig.java
@@ -43,7 +43,7 @@ public class WebSecurityConfig {
     public WebSecurityCustomizer webSecurityCustomizer() {
         // resources 접근 허용 설정
         return (web) -> web.ignoring()
-                .requestMatchers(PathRequest.toH2Console())
+//                .requestMatchers(PathRequest.toH2Console())
                 .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
 
