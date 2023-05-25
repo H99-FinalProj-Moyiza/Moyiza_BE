8fd0b374a3b11b8efc8e665bb46cdc68c0a87de4
Hohomii <glgltjsal@naver.com>
2023-05-25 17:21:50 +0900

    Chore : deploy.sh 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,7 +4,7 @@ REPOSITORY=/home/ubuntu/app
 
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -fla java | grep hayan | awk '{print $1}')
+CURRENT_PID=$(pgrep -fla java | grep Moyiza | awk '{print $1}')
 
 echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
@@ -13,7 +13,7 @@ if [ -z $CURRENT_PID ]; then
 else
   echo "> kill -15 $CURRENT_PID"
   kill -15 $CURRENT_PID
-  sleep 5
+  sleep 10
 fi
 
 echo "> 새 애플리케이션 배포"
@@ -28,4 +28,4 @@ chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
 
-nohup java -jar -Duser.timezone=Asia/Seoul $JAR_NAME >> $REPOSITORY/nohup.out 2>&1 &
+nohup java -jar $JAR_NAME &
