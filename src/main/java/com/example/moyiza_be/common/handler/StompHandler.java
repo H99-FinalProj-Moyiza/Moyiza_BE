32b1fee9e687ef563a7dddbfe85c672493316cea
강재형 <c4save@korea.ac.kr>
2023-06-06 18:01:58 +0900

    Fix : remove postsend method

--- src/main/java/com/example/moyiza_be/common/handler/StompHandler.java
+++ src/main/java/com/example/moyiza_be/common/handler/StompHandler.java
@@ -131,11 +131,4 @@ public class StompHandler implements ChannelInterceptor {
         userPrincipal.setSubscribedChatId(-1L);
         redisCacheService.saveUserInfoToCache(sessionId,userPrincipal);
     }
-
-    @Override
-    public void afterSendCompletion(Message<?> message, MessageChannel channel, boolean sent, Exception ex) {
-        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(message);
-        headerAccessor.setDestination("/chat/" + "4");
-        channel.send(message);
-    }
 }
