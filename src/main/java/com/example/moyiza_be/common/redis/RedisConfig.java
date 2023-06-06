5123a4e21581484a4e1740aff73f69244ae45d55
강재형 <c4save@korea.ac.kr>
2023-06-06 17:32:33 +0900

    Test : test sending subscription message to client

--- src/main/java/com/example/moyiza_be/common/redis/RedisConfig.java
+++ src/main/java/com/example/moyiza_be/common/redis/RedisConfig.java
@@ -13,6 +13,7 @@ import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactor
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.GenericToStringSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
