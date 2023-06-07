5b579a439c960ae053bc11353dad6bd5b5941f3b
강재형 <c4save@korea.ac.kr>
2023-06-07 18:52:11 +0900

    Refactor : ClubService -> goodbyeClub, banClub with method extraction

--- src/main/java/com/example/moyiza_be/club/service/ClubService.java
+++ src/main/java/com/example/moyiza_be/club/service/ClubService.java
@@ -13,10 +13,8 @@ import com.example.moyiza_be.common.enums.CategoryEnum;
 import com.example.moyiza_be.common.enums.ChatTypeEnum;
 import com.example.moyiza_be.common.utils.Message;
 import com.example.moyiza_be.event.entity.Event;
-import com.example.moyiza_be.event.repository.EventRepository;
 import com.example.moyiza_be.event.service.EventService;
 import com.example.moyiza_be.user.entity.User;
-import com.example.moyiza_be.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
@@ -26,10 +24,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDateTime;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -42,27 +37,26 @@ public class ClubService {
     private final EventService eventService;
     private final ClubImageUrlRepository clubImageUrlRepository;
     private final ChatService chatService;
-    private final UserService userService;
     private final ClubJoinEntryRepositoryCustom clubJoinEntryRepositoryCustom;
 
 
     //클럽 가입
     public ResponseEntity<Message> joinClub(Long clubId, User user) {
-        if(clubJoinEntryRepository.existsByClubIdAndUserId(clubId, user.getId())){
+        Club club = loadClubByClubId(clubId);
+
+        if (clubJoinEntryRepository.existsByClubIdAndUserId(clubId, user.getId())) {
             return new ResponseEntity<>(new Message("중복으로 가입할 수 없습니다"), HttpStatus.BAD_REQUEST);
         }
 
-        Club club = clubRepository.findById(clubId).orElse(null);
-        if (club != null) {
-            club.addAttend();
-            clubRepository.save(club);
-        }
+        club.addAttend();
+        clubRepository.save(club);
 
         ClubJoinEntry joinEntry = new ClubJoinEntry(user.getId(), clubId);
         clubJoinEntryRepository.save(joinEntry);
+        chatService.joinChat(clubId, ChatTypeEnum.CLUB, user);
+
         //미래에 조건검증 추가
         Message message = new Message("가입이 승인되었습니다.");
-        chatService.joinChat(clubId, ChatTypeEnum.CLUB, user);
         return ResponseEntity.ok(message);
     }
 
@@ -71,7 +65,7 @@ public class ClubService {
         Page<ClubListResponse> responseList;
         if (category != null && q != null) {
             //카테고리와 검색어를 모두 입력한 경우
-             responseList = clubRepository.findByCategoryAndIsDeletedFalseAndTitleContaining(pageable, category, q).map(ClubListResponse::new);
+            responseList = clubRepository.findByCategoryAndIsDeletedFalseAndTitleContaining(pageable, category, q).map(ClubListResponse::new);
         } else if (category != null) {
             //카테고리만 입력한 경우
             responseList = clubRepository.findByCategoryAndIsDeletedFalse(pageable, category).map(ClubListResponse::new);
@@ -88,7 +82,7 @@ public class ClubService {
     //클럽 상세 조회
     public ResponseEntity<ClubDetailResponse> getClubDetail(Long clubId) {
         Club club = loadClubByClubId(clubId);
-        List<String> clubImageUrlList= clubImageUrlRepository.findAllByClubId(clubId).stream().map(ClubImageUrl::getImageUrl).toList();
+        List<String> clubImageUrlList = clubImageUrlRepository.findAllByClubId(clubId).stream().map(ClubImageUrl::getImageUrl).toList();
         ClubDetailResponse responseDto = new ClubDetailResponse(club, clubImageUrlList);
         return ResponseEntity.ok(responseDto);
     }
@@ -98,74 +92,58 @@ public class ClubService {
     //프로필사진, 닉네임, 클럽 가입 날짜
     public ResponseEntity<List<ClubMemberResponse>> getClubMember(Long clubId) {
 
-        List<ClubMemberResponse> clubMemberResponseList= clubJoinEntryRepositoryCustom.getClubMemberList(clubId);
+        List<ClubMemberResponse> clubMemberResponseList = clubJoinEntryRepositoryCustom.getClubMemberList(clubId);
 
         return ResponseEntity.ok(clubMemberResponseList);
     }
 
     //클럽 탈퇴
     public ResponseEntity<Message> goodbyeClub(Long clubId, User user) {
+        //가입회원수 조회 Query로 할 수 없는지 ? entity에 attend 숫자 없이
+        Club club = loadClubByClubId(clubId);
+        ClubJoinEntry clubJoinEntry = loadClubJoinEntry(user.getId(), clubId);
 
-        ClubJoinEntry joinEntry = clubJoinEntryRepository.findByUserIdAndClubId(user.getId(), clubId);
-        if (joinEntry != null) {
-            clubJoinEntryRepository.delete(joinEntry);
-            Message message = new Message("클럽에서 탈퇴되었습니다.");
-
-            Club club = clubRepository.findById(clubId).orElse(null);
-            if (club != null) {
-                club.cancelAttend();
-                clubRepository.save(club);
-            }
-
-            chatService.leaveChat(clubId, ChatTypeEnum.CLUB, user);
-            return ResponseEntity.ok(message);
-        } else {
-            Message message = new Message("클럽이 존재하지 않거나, 가입 정보가 없습니다.");
-            return ResponseEntity.ok(message);
-        }
+        clubJoinEntryRepository.delete(clubJoinEntry);
+        club.cancelAttend();
+        clubRepository.saveAndFlush(club);
+        chatService.leaveChat(clubId, ChatTypeEnum.CLUB, user);
+        Message message = new Message("클럽에서 탈퇴되었습니다.");
+        return ResponseEntity.ok(message);
     }
 
     //클럽 강퇴
     public ResponseEntity<Message> banClub(Long clubId, User user, BanRequest banRequest) {
-        if(!clubRepository.existsByIdAndIsDeletedFalseAndOwnerIdEquals(clubId, user.getId())){
-            return new ResponseEntity<>(new Message("권한이 없거나, 클럽이 없습니다"), HttpStatus.BAD_REQUEST);
+        if (!clubRepository.existsByIdAndIsDeletedFalseAndOwnerIdEquals(clubId, user.getId())) {
+            throw new IllegalAccessError("권한이 없습니다");
         }
-        ClubJoinEntry joinEntry = clubJoinEntryRepository.findByUserIdAndClubId(banRequest.getBanUserId(), clubId);
-        if (joinEntry != null) {
-            clubJoinEntryRepository.delete(joinEntry);
-            log.info("user " + user.getId() + " banned user " + banRequest.getBanUserId() + " from club " + clubId);
+        Club club = loadClubByClubId(clubId);
+        ClubJoinEntry joinEntry = loadClubJoinEntry(banRequest.getBanUserId(), clubId);
 
-            Club club = clubRepository.findById(clubId).orElse(null);
-            if (club != null) {
-                club.cancelAttend();
-                clubRepository.save(club);
-            }
+        clubJoinEntryRepository.delete(joinEntry);
+        club.cancelAttend();
+        clubRepository.saveAndFlush(club);
+        chatService.leaveChat(clubId, ChatTypeEnum.CLUB, user);
 
-            //추방 후 가입 제한 추가시 여기에 logic
-            Message message = new Message(String.format("user %d 가 클럽에서 강퇴되었습니다",banRequest.getBanUserId()));
-            chatService.leaveChat(clubId, ChatTypeEnum.CLUB, user);
-            return ResponseEntity.ok(message);
-        } else {
-            Message message = new Message("클럽 가입 정보가 없습니다.");
-            return ResponseEntity.ok(message);
-        }
+        log.info("user " + user.getId() + " banned user " + banRequest.getBanUserId() + " from club " + clubId);
+
+        //추방 후 가입 제한 추가시 여기에 logic
+        Message message = new Message(String.format("user %d 가 클럽에서 강퇴되었습니다", banRequest.getBanUserId()));
+        return ResponseEntity.ok(message);
     }
 
     //클럽 생성
-    public ClubDetailResponse createClub(ConfirmClubCreationDto creationRequest, User user){
+    public ClubDetailResponse createClub(ConfirmClubCreationDto creationRequest, User user) {
         Club club = new Club(creationRequest);
         clubRepository.saveAndFlush(club);
         chatService.makeChat(club.getId(), ChatTypeEnum.CLUB, club.getTitle());
         joinClub(club.getId(), user);
         List<String> clubImageUrlList = clubImageUrlRepository.findAllByClubId(creationRequest.getCreateClubId())
                 .stream()
-                .peek(image->image.setClubId(club.getId()))
+                .peek(image -> image.setClubId(club.getId()))
                 .map(ClubImageUrl::getImageUrl)
                 .toList();
         return new ClubDetailResponse(club, clubImageUrlList); // querydsl에서 List로 projection이 가능한가 확인해봐야함
     }
-
-    ////////////////////private method///////////////////////
 
     public ResponseEntity<List<Event>> getClubEventList(User user, Long clubId) {
         List<Event> eventList = eventService.getEventList(clubId);
@@ -175,17 +153,13 @@ public class ClubService {
     public ResponseEntity<Message> deleteClub(User user, Long clubId) {
         //임시구현, 로직 변경 필요할듯 (softdelete ? orphanremoval ?
         Club club = loadClubByClubId(clubId);
-        if(!club.getOwnerId().equals(user.getId())){
+        if (!club.getOwnerId().equals(user.getId())) {
             return new ResponseEntity<>(new Message("내 클럽이 아닙니다"), HttpStatus.UNAUTHORIZED);
-        }
-        else{
+        } else {
             club.flagDeleted(true);
             return ResponseEntity.ok(new Message("삭제되었습니다"));
         }
     }
-
-
-
 
 
     /////////////////////private method///////////////////////
@@ -194,14 +168,19 @@ public class ClubService {
 
     private Club loadClubByClubId(Long clubId) {
         Club club = clubRepository.findById(clubId).orElse(null);
-        if(club == null){
+        if (club == null) {
             log.info("failed to find Club with id : " + clubId);
             throw new NullPointerException("해당 클럽을 찾을 수 없습니다");
-        }
-        else if(club.getIsDeleted().equals(true)){
+        } else if (club.getIsDeleted().equals(true)) {
             throw new NullPointerException("삭제된 클럽입니다");
         }
         return club;
+    }
+
+    private ClubJoinEntry loadClubJoinEntry(Long userId, Long clubId) {
+        return clubJoinEntryRepository.findByUserIdAndClubId(userId, clubId).orElseThrow(
+                () -> new NullPointerException("유저 클럽 참여정보를 찾을 수 없습니다")
+        );
     }
 
     public Integer userOwnedClubCount(Long userId) {
