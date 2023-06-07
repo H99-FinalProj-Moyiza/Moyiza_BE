7ed4649ac13b72615aa4221b573a00d9b0ca3bf4
Hohomii <glgltjsal@naver.com>
2023-06-07 20:33:16 +0900

    Chore : change servletConfig

--- src/main/java/com/example/moyiza_be/config/ServletConfig.java
+++ src/main/java/com/example/moyiza_be/config/ServletConfig.java
@@ -4,6 +4,7 @@ package com.example.moyiza_be.config;
 import org.apache.catalina.connector.Connector;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,15 +12,13 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ServletConfig {
     @Bean
-    public ServletWebServerFactory serverFactory() {
-        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
-        tomcat.addAdditionalTomcatConnectors(createStandardConnector());
-        return tomcat;
+    public WebServerFactoryCustomizer<TomcatServletWebServerFactory> connectorCustomizer() {
+        return (tomcat) -> tomcat.addAdditionalTomcatConnectors(createStandardConnector());
     }
 
     private Connector createStandardConnector () {
-        Connector connector = new Connector("org.apache.coyote.http11NioProtocol");
-        connector.setPort(8081);
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setPort(8080);
         return connector;
     }
 }
