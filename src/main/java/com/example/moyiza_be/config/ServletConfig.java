2bd553a3b450a1a84560b79c50e3f2ac05185062
Hohomii <glgltjsal@naver.com>
2023-06-07 20:02:22 +0900

    Feat : add ServletConfig for http/https multi connect

---
+++ src/main/java/com/example/moyiza_be/config/ServletConfig.java
@@ -0,0 +1,29 @@
+package com.example.moyiza_be.config;
+
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ServletConfig {
+
+    @Value("${server.http.port}")
+    private int httpPort;
+
+    @Bean
+    public ServletWebServerFactory serverFactory() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.addAdditionalTomcatConnectors(createStandardConnector());
+        return tomcat;
+    }
+
+    private Connector createStandardConnector () {
+        Connector connector = new Connector("org.apache.coyote.http11NioProtocol");
+        connector.setPort(httpPort);
+        return connector;
+    }
+}
