56d5001a83996bc0e20f2620a901d9e90738c8ca
Dunkin00 <127109353+Dunkin00@users.noreply.github.com>
2023-06-09 02:17:28 +0900

    Fix : /oneday/create POST -> Cannot invoke--- 404 Error Fix
    - OneDayCreate.getCategory()가 null 값을 반환하여 에러 발생
    - enum type 변수들에(enum != null ? getValue : null )추가

--- src/main/java/com/example/moyiza_be/oneday/dto/OneDayNearByResponseDto.java
+++ src/main/java/com/example/moyiza_be/oneday/dto/OneDayNearByResponseDto.java
@@ -1,12 +1,60 @@
 package com.example.moyiza_be.oneday.dto;
 
+import com.example.moyiza_be.common.enums.GenderPolicyEnum;
+import com.example.moyiza_be.common.enums.OneDayTypeEnum;
+import com.example.moyiza_be.common.enums.TagEnum;
 import com.example.moyiza_be.oneday.entity.OneDay;
+import jakarta.persistence.Column;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 @AllArgsConstructor
 public class OneDayNearByResponseDto {
-    private OneDay oneDay;
+
+    private Long oneDayId;
+    private Long ownerName;
+    private String oneDayTitle;
+    private String oneDayContent;
+    private String oneDayLocation;
+    private double oneDayLatitude;
+    private double oneDayLongitude;
+    private LocalDateTime oneDayStartTime;
+    private String oneDayCategory;
+    private List<String> oneDayTag;
+    private Integer agePolicy;
+    private GenderPolicyEnum genderPolicy;
+    private Integer oneDayGroupSize;
+    private Integer attendantsNum;
+    private String oneDayImage;
+    private OneDayTypeEnum type;
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
     private double distance;
+
+    public OneDayNearByResponseDto(OneDay oneDay, double distance) {
+        this.oneDayId = oneDay.getId();
+        this.ownerName = oneDay.getOwnerId();
+        this.oneDayTitle = oneDay.getOneDayTitle();
+        this.oneDayContent = oneDay.getOneDayContent();
+        this.oneDayLocation = oneDay.getOneDayLocation();
+        this.oneDayLatitude = oneDay.getOneDayLatitude();
+        this.oneDayLongitude = oneDay.getOneDayLongitude();
+        this.oneDayStartTime = oneDay.getOneDayStartTime();
+        this.oneDayCategory = oneDay.getCategory().getCategory();
+        this.oneDayTag = TagEnum.parseTag(oneDay.getTagString());
+        this.agePolicy = oneDay.getAgePolicy();
+        this.genderPolicy = oneDay.getGenderPolicy();
+        this.oneDayGroupSize = oneDay.getOneDayGroupSize();
+        this.attendantsNum = oneDay.getAttendantsNum();
+        this.oneDayImage = oneDay.getOneDayImage();
+        this.type = oneDay.getType();
+        this.createdAt = oneDay.getCreatedAt();
+        this.modifiedAt = oneDay.getModifiedAt();
+        this.distance = distance;
+    }
+
 }
--- src/main/java/com/example/moyiza_be/oneday/dto/onedaycreate/OneDayCreatingResponseDto.java
+++ src/main/java/com/example/moyiza_be/oneday/dto/onedaycreate/OneDayCreatingResponseDto.java
@@ -18,7 +18,7 @@ public class OneDayCreatingResponseDto {
     private final String category;
     private final List<String> tag;
     private final LocalDateTime oneDayStartTime;
-    private final int oneDayGroupSize;
+    private final Integer oneDayGroupSize;
     private final String gender;
     private final Integer age;
     private final String type;
@@ -28,16 +28,16 @@ public class OneDayCreatingResponseDto {
     public OneDayCreatingResponseDto(OneDayCreate oneDay) {
         this.oneDayTitle = oneDay.getOneDayTitle();
         this.oneDayContent = oneDay.getOneDayContent();
-        this.category = oneDay.getCategory().getCategory();
+        this.category = (oneDay.getCategory() != null) ? oneDay.getCategory().getCategory() : null;
         this.tag = TagEnum.parseTag(oneDay.getTagString());
         this.oneDayLocation = oneDay.getOneDayLocation();
         this.oneDayLatitude = oneDay.getOneDayLatitude();
         this.oneDayLongitude = oneDay.getOneDayLongitude();
         this.oneDayStartTime = oneDay.getOneDayStartTime();
         this.oneDayGroupSize = oneDay.getOneDayGroupSize();
-        this.gender = oneDay.getGenderPolicy().getGenderPolicy();
+        this.gender = (oneDay.getGenderPolicy() != null) ? oneDay.getGenderPolicy().getGenderPolicy() : null;
         this.age = oneDay.getAgePolicy();
-        this.type = oneDay.getOneDayType().getOneDayType();
+        this.type = (oneDay.getOneDayType() != null) ? oneDay.getOneDayType().getOneDayType() : null;
         this.image = oneDay.getOneDayImage();
     }
 }
