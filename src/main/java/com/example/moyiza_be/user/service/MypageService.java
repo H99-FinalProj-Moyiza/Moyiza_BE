97914a9c1104d45ffd1a59ee84cc2c01b2d9fc87
Hohomii <127713815+Hohomii@users.noreply.github.com>
2023-06-10 12:51:05 +0900

    Update MypageService.java

--- src/main/java/com/example/moyiza_be/user/service/MypageService.java
+++ src/main/java/com/example/moyiza_be/user/service/MypageService.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class MypageService {
     private final OneDayService oneDayService;
     private final ClubService clubService;
