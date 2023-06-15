382adc047626fe9f392e2c05f0de84bd74b8353d
Dunkin00 <127109353+Dunkin00@users.noreply.github.com>
2023-06-16 01:56:41 +0900

    Refactor : User api url 수정
    - /user 제거
    - CookieUtil setSecure 설정

--- src/main/java/com/example/moyiza_be/common/security/jwt/CookieUtil.java
+++ src/main/java/com/example/moyiza_be/common/security/jwt/CookieUtil.java
@@ -3,6 +3,7 @@ package com.example.moyiza_be.common.security.jwt;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.hibernate.Remove;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
@@ -26,7 +27,7 @@ public class CookieUtil {
     public void addCookie(HttpServletResponse response, String name, String value) {
         Cookie cookie = new Cookie(name, value);
         cookie.setPath("/");
-//        cookie.setSecure(true); Remove this comments when applying Https after certificate issuance
+        cookie.setSecure(true); //Remove this comments when applying Https after certificate issuance
         cookie.setHttpOnly(true);
         cookie.setMaxAge(14 * 24 * 60 * 60); //Cookie expiration period 14 days
         response.addCookie(cookie);
